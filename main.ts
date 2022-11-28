17982f0a3630f7dc3bda30d6787b3ffb7f69cf40
D-A-N-I-E-L1 <119342244+D-A-N-I-E-L1@users.noreply.github.com>
2022-11-28 09:09:09 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,46 @@
+basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # # # . .
+            # . . # .
+            # . . # .
+            # . . # .
+            # # # . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . # # # .
+            . # . # .
+            . # . # .
+            `)
+        basic.showLeds(`
+            # . . . #
+            # # . . #
+            # . # . #
+            # . . # #
+            # . . . #
+            `)
+        basic.showLeds(`
+            . # # # .
+            . . # . .
+            . . # . .
+            . . # . .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # . .
+            . # . . .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . # . . .
+            . # . . .
+            . # . . .
+            . # . . .
+            . # # # .
+            `)
+    }
+})
